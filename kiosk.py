a35c3780de2afa3e097ccb9dbb2c09cc060fde24
Noël Kneib <noel.kneib@siveo.fr>
2018-05-09 09:09:55 +0200

    (kiosk) update the repository

--- kiosk.py
+++ kiosk.py
@@ -22,7 +22,12 @@
 # MA 02110-1301, USA.
 
 from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QListWidgetItem
+
+from models import Package
+from views.custom_package_item import CustomPackageWidget
 from views.kiosk import kiosk_main_view
+import re
 
 
 class Kiosk(QWidget):
@@ -33,4 +38,35 @@ class Kiosk(QWidget):
         super().__init__()
         self.criterion = criterion
 
+        self.packages_list = self.result_list = Package.get_all(self)
+
         kiosk_main_view(self)
+        self.searchbar.textChanged.connect(self.filter_packages)
+
+    def filter_packages(self):
+        if self.searchbar.text() != self.criterion:
+            self.criterion = self.searchbar.text()
+
+        if self.searchbar.text() == "":
+            self.result_list = self.packages_list
+        else:
+            self.result_list = []
+            for package in self.packages_list:
+                if re.search(self.criterion, package.name, flags=re.IGNORECASE) and self.criterion != "":
+                    self.result_list.append(package)
+
+        for element in self.items_list:
+            # Delete all the element in the list
+            self.list.removeItemWidget(element['item'])
+
+        # Regenerate the list
+        for package in self.result_list:
+            self.items_list.append({'item': QListWidgetItem(self.list),
+                                   'item_package': CustomPackageWidget(package)})
+
+        # Attach each item to the list
+        for element in self.items_list:
+            element['item'].setSizeHint(element['item_package'].sizeHint())
+
+            self.list.addItem(element['item'])
+            self.list.setItemWidget(element['item'], element['item_package'])
