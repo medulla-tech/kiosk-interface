b9b031bbda33f22671b430694ea19a5c4fe74d1c
botheis <noel.kneib@siveo.net>
2018-06-05 16:59:51 +0200

    (kiosk) desactivate the install button when it is clicked

--- kiosk_interface/views/custom_package_item.py
+++ kiosk_interface/views/custom_package_item.py
@@ -87,17 +87,18 @@ class CustomPackageWidget(QWidget):
         self.show()
 
         if "Install" in self.actions:
-            self.action_button["Install"].clicked.connect(lambda: self.return_message("Install"))
+            self.action_button["Install"].clicked.connect(lambda: self.return_message(self.action_button["Install"], "Install"))
         if "Ask" in self.actions:
-            self.action_button["Ask"].clicked.connect(lambda: self.return_message("Ask"))
+            self.action_button["Ask"].clicked.connect(lambda: self.return_message(self.action_button["Ask"], "Ask"))
         if "Update" in self.actions:
-            self.action_button["Update"].clicked.connect(lambda: self.return_message("Update"))
+            self.action_button["Update"].clicked.connect(lambda: self.return_message(self.action_button["Update"], "Update"))
         if "Delete" in self.actions:
-            self.action_button["Delete"].clicked.connect(lambda: self.return_message("Delete"))
+            self.action_button["Delete"].clicked.connect(lambda: self.return_message(self.action_button["Delete"], "Delete"))
         if "Launch" in self.actions:
-            self.action_button["Launch"].clicked.connect(lambda: self.return_message("Launch"))
+            self.action_button["Launch"].clicked.connect(lambda: self.return_message(self.action_button["Launch"], "Launch"))
 
-    def return_message(self, action):
+    def return_message(self, button, action):
+        button.setEnabled(False)
         self._message = """{"uuid": "%s", "action": "kioskinterface%s", "subaction": "%s"}"""% (self.uuid, \
                                                                                                  action, action)
         send_message_to_am(self._message)
