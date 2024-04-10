c460483bdeccb8a9e64e971e079eac582781a8c1
botheis <noel.kneib@siveo.net>
2024-04-10 22:48:23 +0200

    (PyQt6) Replace old references
    (cherry picked from commit 32052d08133b28b770b5621b90fca471e02d1d22)

--- kiosk_interface/views/date_picker.py
+++ kiosk_interface/views/date_picker.py
@@ -75,7 +75,7 @@ class DatePickerWidget(QWidget):
         #
         self.ref_button.setEnabled(False)
         self.setWindowFlags(
-            Qt.CustomizeWindowHint | Qt.WindowTitleHint | Qt.WindowStaysOnTopHint
+            Qt.WindowType.CustomizeWindowHint| Qt.WindowType.WindowTitleHint | Qt.WindowType.WindowStaysOnTopHint
         )
 
         #
@@ -196,12 +196,12 @@ class DatePickerWidget(QWidget):
         self.datetime_current = QDateTime(
             self.date_today,
             QTime(int(self.hour_current[0]), int(self.hour_current[1])),
-            Qt.LocalTime,
+            Qt.TimeSpec.LocalTime,
         ).toUTC()
         self.datetime_selected = QDateTime(
             self.date_selected,
             QTime(int(self.hour_selected[0]), int(self.hour_selected[1])),
-            Qt.LocalTime,
+            Qt.TimeSpec.LocalTime,
         ).toUTC()
         self.tuple_current = (
             self.datetime_current.date().year(),
