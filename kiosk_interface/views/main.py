cc7fe5c92e69dd5dbab4088e38ee3cb90fd6363f
SPO <spointu@medulla-tech.io>
2026-06-03 10:56:27 +0200

    (licence) Add missing headers

--- kiosk_interface/views/main.py
+++ kiosk_interface/views/main.py
@@ -1,6 +1,9 @@
-"""
-Application de chat moderne avec système de tickets
-"""
+#!/usr/bin/python3
+# coding: utf-8
+# SPDX-FileCopyrightText: 2016-2023 Siveo, http://www.siveo.net
+# SPDX-FileCopyrightText: 2024-2026 Medulla / Natsu, http://www.medulla-tech.io
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 from dataclasses import dataclass, field
 from datetime import datetime
 import threading
